ad74897b3d817822c7a7d44d7e50fae09fda8d7c
rohancodexp <rohanm6835@gmail.com>
2026-01-20 19:12:59 +0530

    Ignore Next.js build artifacts

--- dashboard/.next/dev/server/chunks/ssr/dashboard__next-internal_server_app_auth_page_actions_2a0ede97.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/dashboard/.next-internal/server/app/auth/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=dashboard__next-internal_server_app_auth_page_actions_2a0ede97.js.map
--- dashboard/.next/dev/static/chunks/dashboard_src_app_page_tsx_0bed05b0._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/9f1d4_next_dist_2b8c1b36._.js"
-],
-    source: "dynamic"
-});
